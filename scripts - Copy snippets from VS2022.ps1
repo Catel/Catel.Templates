f1a0cb7915958c6c1f3c8b3296aa3683baac585a
Geert van Horrik <geert@catenalogic.com>
2022-09-20 08:59:39 +0200

    Add VS 2022 scripts

---
+++ scripts - Copy snippets from VS2022.ps1
@@ -0,0 +1,28 @@
+# This script copies the item and project templates from 
+# %documents%\Visual Studio 2022\Templates to the /templates
+# directory so they can be checked in into source control
+
+$vsVersion = "2022"
+$vsName = "Visual Studio " + $vsVersion
+$vsSnippetsPath = [Environment]::GetFolderPath('MyDocuments') + "\" + $vsName + "\Code Snippets\Visual C#\My Code Snippets\Catel\"
+$sourceDirectory = "snippets\C#\"
+
+$snippets = Get-ChildItem -Path $vsSnippetsPath -File -Name
+
+Write-Host Using snippets path: $vsSnippetsPath
+
+foreach ($snippet in $snippets)
+{
+    $source = $vsSnippetsPath + $snippet
+    $target = $sourceDirectory + $snippet
+
+    Write-Host Copying snippet $snippet to $target
+
+    if (Test-Path $target)
+    {
+        Get-ChildItem -Path $target -Recurse | Remove-Item -force -recurse
+        Remove-Item $target
+    }
+    
+    Copy-Item $source -Filter * -Destination $target -Recurse
+}
---
+++ scripts - Copy snippets to VS2022.ps1
@@ -0,0 +1,30 @@
+# This script copies the item and project templates from 
+# /snippets to the %documents%\Visual Studio 2022\Code Snippets
+# directory so they can be edited and tested
+
+$vsVersion = "2022"
+$vsName = "Visual Studio " + $vsVersion
+$vsSnippetsPath = [Environment]::GetFolderPath('MyDocuments') + "\" + $vsName + "\Code Snippets\Visual C#\My Code Snippets\Catel\"
+$sourceDirectory = "snippets\C#\"
+
+$snippets = Get-ChildItem -Path $sourceDirectory -File -Name
+
+Write-Host Using snippets path: $vsSnippetsPath
+
+[system.io.directory]::CreateDirectory($vsSnippetsPath)
+
+foreach ($snippet in $snippets)
+{
+    $source = $sourceDirectory + $snippet
+    $target = $vsSnippetsPath + $snippet
+
+    Write-Host Copying snippet $snippet to $target
+
+    if (Test-Path $target)
+    {
+        Get-ChildItem -Path $target -Recurse | Remove-Item -force -recurse
+        Remove-Item $target
+    }
+    
+    Copy-Item $source -Filter * -Destination $target -Recurse
+}
